268fa8ea70ae5880f9b4d6f3b1fa8717e975de40
Irene Jia <irene.jia@stelligent.com>
2022-01-25 15:52:54 -0800

    Create module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cdk_constructs',
+    version='0.1',    
+    description='test construct modules',
+    author='Irene Jia',
+    author_email='irene.jia@stelligent.com',
+    license='MIT',
+    packages=['cdk_constructs'],
+    install_requires=['aws-cdk-lib==2.0.0',
+                      'constructs>=10.0.0,<11.0.0']
+)
