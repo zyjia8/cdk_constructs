36371eb5eb157cc7bf689910c879b632e34fc96e
Irene Jia <irene.jia@stelligent.com>
2022-01-26 17:00:13 -0800

    Use cfn resources

--- cdk_constructs/sns_construct.py
+++ cdk_constructs/sns_construct.py
@@ -8,5 +8,10 @@ class NotifyingTopic(Construct):
 
     def __init__(self, scope: Construct, id: str, tname: str):
         super().__init__(scope, id)
-        self.topic = sns.Topic(self, tname)
+        self.topic = sns.CfnTopic(self, tname,
+            content_based_deduplication=False,
+            display_name=tname,
+            fifo_topic=False,
+            topic_name=tname
+        )
   
